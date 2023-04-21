8b6c9a7d4959e3b2b2f8b2e7bd16a018500fe0fb
amitlahane91 <lahaneamit91@gmail.com>
2023-04-21 18:15:07 +0530

    Revision Class Commit

--- MyJAVA/src/Stringg/Revision.java
+++ MyJAVA/src/Stringg/Revision.java
@@ -1,27 +1,34 @@
 package Stringg;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import java.util.Scanner;
+import java.util.Set;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Revision {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\lahan\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		
-		  String str = "maharashtra";
-		  String revstr = "";
-		  
-		  for(int i=str.length()-1; i>=0; i--) {
-			  revstr += str.charAt(i);
-		  }
-		  System.out.println(revstr);
-		    }
-		}
+		driver.get("https://qa-automation-practice.netlify.app/web-table.html");
+		
+		driver.findElement(By.xpath("(//a[@id='buttons'])[2]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[@id='browser-tab']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//a[@href='web-table.html'])[2]")).click();
+		 Set<String> add = driver.getWindowHandles();
+		System.out.println(add);
+		 
 
 	
-
+	}
+}
 
